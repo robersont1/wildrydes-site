1e45b2aab07ca74a8d540a81e6ae3388a8387882
robersont1 <147337751+robersont1@users.noreply.github.com>
2023-12-03 18:44:51 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-west-2_E8Gqmojj6',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '4920lgj37ij7gba8at1r3cu159',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId:         'us-west-2_tcF6QFI5r',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '2jhd9nqmgubgte4ckra3gsg26s',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region:             'us-west-2'          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          'https://bpm536djyk.execute-api.us-west-2.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
